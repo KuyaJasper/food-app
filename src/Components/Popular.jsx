9f5f239c8632d996f208a73ca81b802aa6d2efc7
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2022-03-14 16:45:39 -0400

    set up API call as JSON

--- src/Components/Popular.jsx
+++ src/Components/Popular.jsx
@@ -1,12 +1,21 @@
+import { useEffect } from "react";
+
 function Popular() {
 
-const getPopular = async () => {
-  const api = await fetch (`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+//useEffect being used to call API when mounting/ loading of application. Array is added to pass data
+  useEffect(() => {
+    getPopular();
+  },[])
+
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+    );
+    const data = await api.json();
+    console.log(data);
+  };
+
+  return <div>Popular</div>;
 }
 
-  return (
-    <div>Popular</div>
-  )
-}
-
-export default Popular
+export default Popular;
