c48416966a948e8b9a2835d7d6ad8cf7ade639a6
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2022-03-14 17:18:39 -0400

    added some styling to cards and set up slider

--- src/Components/Popular.jsx
+++ src/Components/Popular.jsx
@@ -1,17 +1,16 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 // Splide lets me use carosels. https://splidejs.com/integration/react-splide/
-import{Splide, SplideSlide} from '@splidejs/react-splide';
-import '@splidejs/splide/dist/css/splide.min.css';
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 function Popular() {
+  const [popular, setPopular] = useState([]);
 
-const [popular, setPopular]= useState([])
-
-//useEffect being used to call API when mounting/ loading of application. Array is added to pass data
+  //useEffect being used to call API when mounting/ loading of application. Array is added to pass data
   useEffect(() => {
     getPopular();
-  },[])
+  }, []);
 
   const getPopular = async () => {
     const api = await fetch(
@@ -19,40 +18,49 @@ const [popular, setPopular]= useState([])
     );
     const data = await api.json();
     // console.log(data);
-    setPopular(data.recipes)
+    setPopular(data.recipes);
   };
 
-  return <div>
+  return (
+    <div>
       return(
-        <Wrapper>
-          <h3>Popular Picks</h3>
-
-          <Splide>
+      <Wrapper>
+        <h3>Popular Picks</h3>
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
           {popular.map((recipe) => {
-            return(
+            return (
               <SplideSlide>
-              <Card key={recipe.id}>
-                <p>{recipe.title}</p>
-                <img src={recipe.image} alt={recipe.title} />
-              </Card>
+                <Card key={recipe.id}>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                </Card>
               </SplideSlide>
             );
           })}
-          </Splide>
-        </Wrapper>
-      );
-  </div>;
+        </Splide>
+      </Wrapper>
+    </div>
+  );
 }
 
 const Wrapper = styled.div`
   margin: 4rem 0rem;
-`
+`;
 const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
   overflow: hidden;
-  img{
+
+  img {
     border-radius: 2rem;
   }
-`
+`;
 export default Popular;
