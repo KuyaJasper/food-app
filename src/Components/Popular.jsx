1a3841bbf7fc115ffe6e28ec1e66dd4187204624
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2022-03-14 17:31:06 -0400

    added linear gradient to cards

--- src/Components/Popular.jsx
+++ src/Components/Popular.jsx
@@ -23,7 +23,6 @@ function Popular() {
 
   return (
     <div>
-      return(
       <Wrapper>
         <h3>Popular Picks</h3>
         <Splide
@@ -38,9 +37,10 @@ function Popular() {
           {popular.map((recipe) => {
             return (
               <SplideSlide>
-                <Card key={recipe.id}>
+                <Card>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
                 </Card>
               </SplideSlide>
             );
@@ -58,9 +58,39 @@ const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
   overflow: hidden;
+  position: relative;
 
   img {
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
 `;
 export default Popular;
