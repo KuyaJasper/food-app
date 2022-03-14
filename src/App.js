bc1ca3a5f05e81adc5d98112d9e6995ee8749fa6
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2022-03-14 16:39:43 -0400

    set up routes and api key

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import Pages from "./Pages/Pages";
 
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
+      <Pages/>
+
     </div>
   );
 }
--- src/Components/Popular.jsx
+++ src/Components/Popular.jsx
@@ -1,4 +1,9 @@
 function Popular() {
+
+const getPopular = async () => {
+  const api = await fetch (`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+}
+
   return (
     <div>Popular</div>
   )
