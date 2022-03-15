ae7f23bf0ba4ea88d92c282afe13db92bbfb5d4b
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2022-03-14 23:05:41 -0400

    added dependencies to searched.jsx

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 function Search() {
   const [input, setInput] = useState("");
+  const Navigate = useNavigate();
 
   const submitHandler = (e) => {
     e.preventDefault();
+    Navigate("/searched/" + input);
   };
 
   return (
--- src/Pages/Searched.jsx
+++ src/Pages/Searched.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function Searched() {
-  return (
-    <div>Searched</div>
-  )
+  const [searched, setSearched] = useState([]);
+  let params = useParams();
+
+  const getSearched = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`
+    );
+    const recipies = await data.json();
+    setSearched(recipies.results);
+  };
+
+  useEffect(() => {
+    getSearched(params.search);
+  }, [params.search]);
+
+  return <div>Searched</div>;
 }
 
-export default Searched
+export default Searched;
