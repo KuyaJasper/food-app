f7415afa19d98eb4ecafa7012c7d79d8771c7ac9
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2022-03-14 22:57:06 -0400

    styled and set up search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Pages from "./Pages/Pages";
 import Category from "./Components/Category";
+import Search from "./Components/Search";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Search />
         <Category />
         <Pages />
       </BrowserRouter>
