618f55fec6fd4c06c14f4d7645982342d984daa0
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2022-03-14 23:12:02 -0400

    set up recipe route

---
+++ src/Pages/Recipe.jsx
@@ -0,0 +1,14 @@
+import { useEffect,useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+
+import React from 'react'
+
+function Recipe() {
+  return (
+    <div>Recipe</div>
+  )
+}
+
+export default Recipe
